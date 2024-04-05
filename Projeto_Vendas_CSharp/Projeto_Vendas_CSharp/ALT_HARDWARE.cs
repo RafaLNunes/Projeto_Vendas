a8eb965311fa9520e4a37257d4d82391508673a9
Gabriel de Oliveira <gabriel.oliveira00619628@sesisenaipr.org.br>
2024-04-05 11:12:48 -0300

    Design FrmALT_HARDWARE
    A

---
+++ Projeto_Vendas_CSharp/Projeto_Vendas_CSharp/ALT_HARDWARE.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Vendas_CSharp
+{
+    public partial class ALT_HARDWARE : Form
+    {
+        public ALT_HARDWARE()
+        {
+            InitializeComponent();
+        }
+
+        private void SideBar_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
